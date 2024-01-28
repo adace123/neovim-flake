1eda593342dd821d9d35a4060a23cc3dcda4a7eb
aaron.feigenbaum <aaron@enervee.com>
2024-01-28 13:17:36 -0800

    remove flake-parts

--- flake.nix
+++ flake.nix
@@ -18,7 +18,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,52 +26,43 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    tree-sitter-nu = {
+      url = "github:nushell/tree-sitter-nu";
+      flake = false;
+    };
   };
 
   outputs = {
+    self,
     nixpkgs,
     nixvim,
-    flake-parts,
     pre-commit-hooks,
     ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+  } @ inputs: let
+    systems = ["x86_64-linux" "x86_64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    forEachPkgs = f: forEachSystem (system: f (import nixpkgs {inherit system;}));
+  in {
+    packages = forEachPkgs (pkgs: let
+      nvim = nixvim.legacyPackages.${pkgs.system}.makeNixvimWithModule {
+        inherit pkgs;
+        extraSpecialArgs = {inherit inputs;};
+        module = ./config;
+      };
+    in {
+      default = nvim;
+    });
 
-      perSystem = {
-        self',
-        system,
-        pkgs,
-        lib,
-        ...
-      }: let
-        nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-          inherit pkgs;
-          module = ./config;
-        };
-      in {
-        _module.args.pkgs = import nixpkgs {inherit system;};
-
-        checks = {
-          default = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              statix.enable = true;
-              alejandra.enable = true;
-            };
-          };
-        };
-
-        packages.default = nvim;
-
-        devShells.default = pkgs.mkShell {
-          inherit (self'.checks.pre-commit-check) shellHook;
+    checks = forEachPkgs (pkgs: {
+      pre-commit-check = inputs.pre-commit-hooks.lib.${pkgs.system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
+          deadnix.enable = true;
+          statix.enable = true;
         };
       };
-    };
+    });
+    devShells = forEachPkgs (pkgs: {default = pkgs.mkShell {inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;};});
+  };
 }
