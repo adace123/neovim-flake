69465427221b2aced3e58607b08d1c1565d0e0ce
aaron.feigenbaum <aaron@enervee.com>
2024-02-07 14:33:58 -0800

    add vim-just, update lock file

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -35,6 +35,7 @@ in {
   };
   extraPlugins = with pkgs.vimPlugins; [
     nvim-treesitter-textobjects
+    vim-just
   ];
 
   extraFiles = {
@@ -48,78 +49,69 @@ in {
       filetype = "nu",
     }
 
-    require("nvim-treesitter.parsers").get_parser_configs().just = {
-        install_info = {
-          url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
-          files = { "src/parser.c", "src/scanner.c" },
-          branch = "main",
+    require'nvim-treesitter.configs'.setup {
+      textobjects = {
+          select = {
+              enable = true,
+              keymaps = {
+                  ["aa"] = "@parameter.outer",
+                  ["ia"] = "@parameter.inner",
+                  ["af"] = "@function.outer",
+                  ["if"] = "@function.inner",
+                  ["ac"] = "@class.outer",
+                  ["ic"] = "@class.inner",
+                  ["ai"] = "@conditional.outer",
+                  ["ii"] = "@conditional.inner",
+                  ["al"] = "@loop.outer",
+                  ["il"] = "@loop.inner",
+                  ["ak"] = "@block.outer",
+                  ["ik"] = "@block.inner",
+              },
+          },
+
+          move = {
+              enable = true,
+              set_jumps = true,
+              goto_next_start = {
+                  ["]m"] = "@function.outer",
+                  ["]c"] = "@class.outer",
+              },
+              goto_next_end = {
+                  ["]M"] = "@function.outer",
+                  ["]["] = "@class.outer",
+              },
+              goto_previous_start = {
+                  ["[m"] = "@function.outer",
+                  ["[c"] = "@class.outer",
+              },
+              goto_previous_end = {
+                  ["[M"] = "@function.outer",
+                  ["[]"] = "@class.outer",
+              },
+          },
+
+          swap = {
+              enable = true,
+              swap_next = {
+                  [')a'] = '@parameter.inner',
+              },
+              swap_previous = {
+                  [')A'] = '@parameter.inner',
+              },
+          },
       },
-      maintainers = { "@IndianBoy42" },
     }
 
-      require'nvim-treesitter.configs'.setup {
-      	textobjects = {
-      		select = {
-      			enable = true,
-      			keymaps = {
-      				["aa"] = "@parameter.outer",
-      				["ia"] = "@parameter.inner",
-      				["af"] = "@function.outer",
-      				["if"] = "@function.inner",
-      				["ac"] = "@class.outer",
-      				["ic"] = "@class.inner",
-      				["ai"] = "@conditional.outer",
-      				["ii"] = "@conditional.inner",
-      				["al"] = "@loop.outer",
-      				["il"] = "@loop.inner",
-      				["ak"] = "@block.outer",
-      				["ik"] = "@block.inner",
-      			},
-      		},
+    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
-      		move = {
-      			enable = true,
-      			set_jumps = true,
-      			goto_next_start = {
-      				["]m"] = "@function.outer",
-      				["]c"] = "@class.outer",
-      			},
-      			goto_next_end = {
-      				["]M"] = "@function.outer",
-      				["]["] = "@class.outer",
-      			},
-      			goto_previous_start = {
-      				["[m"] = "@function.outer",
-      				["[c"] = "@class.outer",
-      			},
-      			goto_previous_end = {
-      				["[M"] = "@function.outer",
-      				["[]"] = "@class.outer",
-      			},
-      		},
+    -- vim way: ; goes to the direction you were moving.
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
-      		swap = {
-      			enable = true,
-      			swap_next = {
-      				[')a'] = '@parameter.inner',
-      			},
-      			swap_previous = {
-      				[')A'] = '@parameter.inner',
-      			},
-      		},
-      	},
-      }
-
-      local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
-      -- vim way: ; goes to the direction you were moving.
-      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
-      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
   '';
 }
